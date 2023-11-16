dc55db3a5c70f8ddf884bf19894e2d02bfe1ca79
helyson-ti <helysonsantiago2000@gmail.com>
2023-11-16 10:37:41 -0300

    inclusão de quick sort

---
+++ sorting_algorithms/Quick_Sort.java
@@ -0,0 +1,44 @@
+package sorting_algorithms;
+
+public class Quick_Sort/*Hoare*/{
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[(low + high) / 2];
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Array ordenado: ");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
